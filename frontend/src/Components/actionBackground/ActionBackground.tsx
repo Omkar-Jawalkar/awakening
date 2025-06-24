cd6a822353cc4942f9d41c7691e0e396d7b35726
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2025-06-24 21:54:32 +0530

    feat: enhance ActionBackground component layout and add description prop

--- frontend/src/Components/actionBackground/ActionBackground.tsx
+++ frontend/src/Components/actionBackground/ActionBackground.tsx
@@ -1,7 +1,7 @@
 import actionBackgroundImage from "../../assets/action-background.png";
 const ActionBackground = ({
     title = "Notification",
-    // description,
+    description = "lorem ipsum",
     icon = "!!",
     // onClick,
     children = "",
@@ -18,7 +18,7 @@ const ActionBackground = ({
             <div className="z-20 top-[10%] p-2  bg-[#1f1531]/50 w-[650px] h-[450px] border-1 border-violet-400 left-[10%] absolute text-amber-50">
                 <div className="flex flex-col justify-between h-full items-center gap-4">
                     {/* title & description */}
-                    <div>
+                    <div className="flex flex-col justify-center items-center">
                         <div className="text-center opacity-100  mt-12   gap-4 flex justify-center items-center">
                             <h1 className="text-4xl drop-shadow-[0_0_10px_rgba(236,72,153,0.8)]">
                                 {icon}{" "}
@@ -27,8 +27,8 @@ const ActionBackground = ({
                                 {title}
                             </h1>
                         </div>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                        <p className="drop-shadow-[0_0_10px_rgba(236,72,153,0.8)]">
+                            {description}
                         </p>
                     </div>
                     <div>{children}</div>
